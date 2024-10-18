5a8cc45f98364a42725196f8a6a9641b1e1ca09f
Adrian Ochs <15180657+adrianochs@users.noreply.github.com>
2024-10-18 11:17:20 +0200

    sol ch 6

--- cookbook/Chapter 6 - String Operations- Which month was the snowiest.py
+++ cookbook/Chapter 6 - String Operations- Which month was the snowiest.py
@@ -2,20 +2,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import polars as pl
+import seaborn as sns
 
 plt.style.use("ggplot")
 plt.rcParams["figure.figsize"] = (15, 3)
 plt.rcParams["font.family"] = "sans-serif"
 
+DATA_PATH = "../data/weather_2012.csv"
 
 # %%
 # We saw earlier that pandas is really good at dealing with dates. It is also amazing with strings! We're going to go back to our weather data from Chapter 5, here.
-weather_2012 = pd.read_csv(
-    "../data/weather_2012.csv", parse_dates=True, index_col="date_time"
-)
+weather_2012 = pd.read_csv(DATA_PATH, parse_dates=True, index_col="date_time")
 weather_2012[:5]
 
 # TODO: load the data using polars and call the data frame pl_wather_2012
+pl_weather_2012 = pl.read_csv(DATA_PATH, try_parse_dates=True)
 
 
 # %%
@@ -30,7 +32,11 @@ is_snowing.plot()
 plt.show()
 
 # TODO: do the same with polars
-
+pl_weather_description = pl_weather_2012.with_columns(
+    is_snowing=pl.col("weather").str.contains("Snow")
+)
+# pl_weather_description.plot.bar(x="date_time", y="is_snowing")  # does not work bcs. data too big
+sns.barplot(data=pl_weather_description.to_pandas(), x="date_time", y="is_snowing")
 
 # %%
 # If we wanted the median temperature each month, we could use the `resample()` method like this:
@@ -40,6 +46,12 @@ plt.show()
 # Unsurprisingly, July and August are the warmest.
 
 # TODO: and now in Polars
+pl_weather_2012_monthly = pl_weather_2012.group_by_dynamic(
+    "date_time", every="1mo"
+).agg(pl.col("temperature_c").median())
+pl_weather_2012_monthly.head()
+
+pl_weather_2012_monthly.plot.bar(x="date_time", y="temperature_c")
 
 
 # %%
@@ -53,3 +65,9 @@ plt.show()
 # So now we know! In 2012, December was the snowiest month. Also, this graph suggests something that I feel -- it starts snowing pretty abruptly in November, and then tapers off slowly and takes a long time to stop, with the last snow usually being in April or May.
 
 # TODO: please do the same in Polars
+pl_weather_description = pl_weather_description.group_by_dynamic(
+    "date_time", every="1mo"
+).agg(pl.col("is_snowing").mean())
+pl_weather_description.head()
+
+pl_weather_description.plot.bar(x="date_time", y="is_snowing")
